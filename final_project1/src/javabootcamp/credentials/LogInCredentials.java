1936cef3ba8e5a3266ee7fda9b4375345c1194c1
kettyPek <ketty.pekarsky@gmail.com>
2022-04-25 00:43:26 +0300

    LogInCredentials class added

---
+++ final_project1/src/javabootcamp/credentials/LogInCredentials.java
@@ -0,0 +1,16 @@
+package javabootcamp.credentials;
+
+public class LogInCredentials {
+	
+	protected String userName;
+	protected String password;
+	
+	public String getUsername() {
+		return userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+}
