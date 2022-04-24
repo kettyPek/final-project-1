2636b48bc46050616174edeb9a3a8a18d3313167
kettyPek <ketty.pekarsky@gmail.com>
2022-04-25 00:41:35 +0300

    action package added

---
+++ final_project1/src/javabootcamp/actions/ActivityData.java
@@ -0,0 +1,12 @@
+package javabootcamp.actions;
+
+import java.time.LocalDateTime;
+
+public class ActivityData {
+	
+	protected ActivityName activityName;
+	protected double balanceChange;
+	protected LocalDateTime timeStamp;
+	protected String additionalInfo;
+
+}
---
+++ final_project1/src/javabootcamp/actions/ActivityName.java
@@ -0,0 +1,13 @@
+package javabootcamp.actions;
+
+public enum ActivityName {
+	
+	DEPOSIT,
+	WITHDRAWAL,
+	GET_LOAN,
+	MAKE_PAYMENT,
+	TRANSFER,
+	FEE_COLLECTION,
+	PAY_BILL;
+
+}
