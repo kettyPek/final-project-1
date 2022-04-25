4bdd6981d0c396a4ff967b87f06e2eeaa45b4765
kettyPek <ketty.pekarsky@gmail.com>
2022-04-25 13:11:30 +0300

    makeDeposit, setCredentials methods

--- final_project1/src/javabootcamp/person/AccountOwner.java
+++ final_project1/src/javabootcamp/person/AccountOwner.java
@@ -9,17 +9,28 @@ public class AccountOwner extends Person{
 	
 	protected Account account = null;
 	protected double monthlyIncome;
-	protected LogInCredentials credentials;
+	protected LogInCredentials credentials = null;
 	
 	public AccountOwner(String firstName, String lastName, long phoneNumber, LocalDate birthDat, double monthlyIncome) {
 		super(firstName, lastName, phoneNumber, birthDat);
 		this.monthlyIncome = monthlyIncome;
 	}
 	
+	public void setCredentials(String username, String password) {
+		credentials = new LogInCredentials(username, password);
+	}
+	
 	public LogInCredentials getCredentials() {
 		return credentials;
 	}
 	
+	public long getPhoneNumber() {
+		return super.phoneNumber;
+	}
 	
-
+	public void makeDeposit(double depositFunds) {
+		account.deposit(depositFunds);
+		System.out.println("Successful deposit");
+	}
+	
 }
