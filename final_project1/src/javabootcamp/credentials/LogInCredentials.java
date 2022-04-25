04ff54f9dda09d151b3d9c606c73b2d7a1f4e9e0
kettyPek <ketty.pekarsky@gmail.com>
2022-04-25 13:07:41 +0300

    usaernameIsValid , passwordIsValid methods added

--- final_project1/src/javabootcamp/credentials/LogInCredentials.java
+++ final_project1/src/javabootcamp/credentials/LogInCredentials.java
@@ -2,15 +2,50 @@ package javabootcamp.credentials;
 
 public class LogInCredentials {
 	
-	protected String userName;
+	protected String username;
 	protected String password;
+
+	
+	public LogInCredentials(String username,String password) {
+		this.username = username;
+		this.password = password;
+	}
 	
 	public String getUsername() {
-		return userName;
+		return username;
 	}
 	
 	public String getPassword() {
 		return password;
 	}
-
+	
+	public static boolean usernameIsValid(String username) {
+		char ch ;
+		for(int i=0; i<username.length(); i++) { 
+			ch = username.charAt(i);
+			if(ch<'0'||ch>'9' )
+				if( ch<'a'|| ch>'z')
+					if(ch<'A'|| ch>'B')
+						return false;
+		}
+		return true;
+	}
+	
+	public static boolean passwordIsValid(String password) {
+		if(password.length()<4 || password.length()>8)
+			return false;
+		char ch ;
+		int countDigit = 0, countLetter = 0;
+		for(int i=0; i<password.length(); i++) { 
+			ch = password.charAt(i);
+			if(ch>='0'&&ch<='9' )
+				countDigit++;
+			else if(ch>='a'&&ch<='z' || ch>='A'&&ch<='B')
+					countLetter++;
+		}
+		if(countDigit==0 || countLetter==0)
+			return false;
+		return true;
+	}
+	
 }
