6e5ba8b47ce4778672f8bc3230b7b02f3cbb552b
kettyPek <ketty.pekarsky@gmail.com>
2022-04-25 13:09:22 +0300

    deposit method added

--- final_project1/src/javabootcamp/account/Account.java
+++ final_project1/src/javabootcamp/account/Account.java
@@ -3,10 +3,25 @@ package javabootcamp.account;
 import javabootcamp.actions.ActivityData;
 
 public class Account {
-	
+	//TODO enter id field
 	protected double balance;
 	protected AccountType accountType; 
 	protected ActivityData [] activityLog;
+	
+	public Account(double balance,AccountType accountType) {
+		this.balance = balance;
+		this.accountType = accountType;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double depositFunds) {
+		this.balance += depositFunds;
+	}
+	
+	
 	
 
 }
