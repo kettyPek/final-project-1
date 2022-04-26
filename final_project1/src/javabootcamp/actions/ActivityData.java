2cc25319f90b0af21fcf5b033f3fe9a95e39d2fe
kettyPek <ketty.pekarsky@gmail.com>
2022-04-26 03:34:20 +0300

    constructor updated

--- final_project1/src/javabootcamp/actions/ActivityData.java
+++ final_project1/src/javabootcamp/actions/ActivityData.java
@@ -8,5 +8,24 @@ public class ActivityData {
 	protected double balanceChange;
 	protected LocalDateTime timeStamp;
 	protected String additionalInfo;
+	
+	public ActivityData(ActivityName activityName,double balanceChange,String additionalInfo) {
+		this.activityName = activityName;
+		this.balanceChange = balanceChange;
+		this.timeStamp = LocalDateTime.now();
+		this.additionalInfo = additionalInfo;
+	}
+	
+	public LocalDateTime getTimeStemp() {
+		return timeStamp;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity: "+ activityName + ", balance change: " + balanceChange + ", time stamp: "
+				+ timeStamp + ", additiona info: " + additionalInfo;
+	}
+	
+	
 
 }
