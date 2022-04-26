b84820dacc14cb867907128fe92a7f267427ee9c
kettyPek <ketty.pekarsky@gmail.com>
2022-04-26 03:31:38 +0300

    addToBalance, subtractFromBalance,addNewActivity methods added

--- final_project1/src/javabootcamp/account/Account.java
+++ final_project1/src/javabootcamp/account/Account.java
@@ -1,6 +1,10 @@
 package javabootcamp.account;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import javabootcamp.actions.ActivityData;
+import javabootcamp.actions.ActivityName;
 
 public class Account {
 	//TODO enter id field
@@ -40,10 +44,56 @@ public class Account {
 	public AccountType getAccountType() {
 		return accountType;
 	}
-		
-	public void deposit(double depositFunds) {
-		this.balance += depositFunds;
+	
+ 	//
+	/**
+	 * Add new activity to activityLog array
+	 * @param activity - new activity to add
+	 */
+	private void addActivityToActivityLog(ActivityData activity) {
+		ActivityData [] updatedActivityLog = new ActivityData[activityLog.length+1];
+		for(int i=0; i<activityLog.length; i++)
+			updatedActivityLog[i] = activityLog[i];
+		updatedActivityLog[activityLog.length] = activity;
+		activityLog = updatedActivityLog;
 	}
+	
+	public void produceActivityReportFromDate(LocalDate startDate) {
+		int index = findIndexOfStartDate(startDate);
+		if(index==-1)
+			System.out.println("No activities from this date.");
+		else 
+			displayActivitiesFromIndex(index);	
+	}
+	
+	private int findIndexOfStartDate(LocalDate startDate) {
+		for(int i=0; i<activityLog.length;i++)
+			if(activityLog[i].getTimeStemp().toLocalDate().equals(startDate))
+				return i;
+		return -1;
+	}
+	
+	private void displayActivitiesFromIndex(int index) {
+		for(int i=index; i<activityLog.length; i++)
+			System.out.println(activityLog[i].toString());
+	}
+	
+	public void addToBalance(double amountToAdd) {
+		balance += amountToAdd;
+	}
+	
+	public void subtractFromBalance(double amountToSubtract) {
+		balance -= amountToSubtract;
+	}
+	
+	public void addNewActivity(ActivityName activityName,double balanceChange,String additionalInfo) {
+		ActivityData activity = new ActivityData(activityName,balanceChange,additionalInfo);
+		addActivityToActivityLog(activity);	
+	}
+	
+	
+	
+		
 	
 	
 	
