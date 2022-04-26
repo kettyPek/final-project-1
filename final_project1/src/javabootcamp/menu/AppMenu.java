b98d272c2b998f0a79c231459a0ce76aaae641b2
kettyPek <ketty.pekarsky@gmail.com>
2022-04-26 16:39:13 +0300

    activateUserMenu method added

--- final_project1/src/javabootcamp/menu/AppMenu.java
+++ final_project1/src/javabootcamp/menu/AppMenu.java
@@ -1,10 +1,36 @@
 package javabootcamp.menu;
+
+import javabootcamp.person.AccountOwner;
+import javabootcamp.utils.Utils;
+
 /**
  * Contains the presented menus in the application
  * @author ketty
  *
  */
-public class AppMenu {
+public abstract class AppMenu {
+	
+	 public static boolean mainMenuActivated;
+	 public static boolean accountOwnerMenuActivated;
+	 
+	 static {
+		 setMenusStatus(true,false);
+	 }
+	 
+	 private static void setMenusStatus(boolean mainMenuAtatus,boolean accountOwnerMenuStatus) {
+		 mainMenuActivated = mainMenuAtatus;
+		 accountOwnerMenuActivated = accountOwnerMenuStatus;
+		 
+	 }
+	 
+	 public static boolean getMainMenuActivated() {
+		 return mainMenuActivated;
+	 }
+	 
+	 public static boolean getAccountOwnerMenuActivatedd() {
+		 return accountOwnerMenuActivated;
+	 }
+
 	
 	/**
 	 * Displays main options menu
@@ -13,7 +39,7 @@ public class AppMenu {
 		System.out.println("please choose:");
 		System.out.println("1 - log in");
 		System.out.println("2 - open new account");
-		System.out.println("3 - log out");
+		System.out.println("3 - Exit app");
 	}
 	
 	/**
@@ -27,6 +53,8 @@ public class AppMenu {
 		System.out.println("4 - withdrawal");
 		System.out.println("5 - transfer funds");
 		System.out.println("6 - pay bill");
+		System.out.println("7 - get loan");
+		System.out.println("8 - log out");
 	}
 	
 	/**
@@ -35,5 +63,25 @@ public class AppMenu {
 	public static void displayBankManagerMenu() {
 		System.out.println("please choose:");
 		System.out.println("1 - approve applicants");
+		System.out.println("2 - produce log report");
+		System.out.println("3 - log out");
 	}
+	
+	public static void exitMainMenu() {
+		setMenusStatus(false,false);
+	}
+	
+	public static void exitAccountOwnerMenu() {
+		setMenusStatus(true,false);
+	}
+	
+	public static void activateUserMenu(AccountOwner user) {
+		accountOwnerMenuActivated = true;
+		while(accountOwnerMenuActivated) {
+			user.displaySelcetionMenu();
+			int selection = Utils.scanner.nextInt();
+			user.activateUserSelection(selection);
+		}
+	}
+	
 }
