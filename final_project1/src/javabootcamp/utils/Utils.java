d3d902c9538b799c45d89af8220687ba057df584
kettyPek <ketty.pekarsky@gmail.com>
2022-04-26 21:21:50 +0300

    Documentation added

--- final_project1/src/javabootcamp/utils/Utils.java
+++ final_project1/src/javabootcamp/utils/Utils.java
@@ -2,7 +2,11 @@ package javabootcamp.utils;
 
 import java.time.LocalDate;
 import java.util.Scanner;
-
+/**
+ * Contains Scanner object and static methods
+ * @author ketty
+ *
+ */
 public abstract class Utils {
 	
 	public static Scanner scanner = new Scanner(System.in);
@@ -18,24 +22,44 @@ public abstract class Utils {
 		return date;
 	}
 	
+	/**
+	 * Receive string from the user which represent object to scan, and scans integer
+	 * @param type - object to scan
+	 * @return scanned integer
+	 */
 	public static int scanIntOf(String type) {
 		System.out.println("Enter " + type);
 		int userInput = scanner.nextInt();
 		return userInput;
 	}
 	
+	/**
+	 * Receive string from the user which represent object to scan, and scans double 
+	 * @param type - object to scan
+	 * @return scanned double
+	 */
 	public static double scanDoubleOf(String type) {
 		System.out.println("Enter " + type);
 		double userInput = scanner.nextDouble();
 		return userInput;
 	}
 	
+	/**
+	 * Receive string from the user which represent object to scan, and scans String 
+	 * @param type - object to scan
+	 * @return scanned String
+	 */
 	public static String scanStringOf(String type) {
 		System.out.println("Enter " + type);
 		String userInput = scanner.next();
 		return userInput;
 	}
 	
+	/**
+	 * Receive string from the user which represent object to scan, and scans Long 
+	 * @param type - object to scan
+	 * @return scanned long
+	 */
 	public static long scanLongOf(String type) {
 		System.out.println("Enter " + type);
 		long userInput = scanner.nextLong();
